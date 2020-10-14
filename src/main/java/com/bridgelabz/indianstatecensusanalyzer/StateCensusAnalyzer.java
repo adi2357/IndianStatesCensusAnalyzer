ddb4217ed873a1a8cf0b567815e8f909a3ea0422
Aditya Verma <aaadarths2357@gmail.com>
2020-10-14 19:10:43 +0530

    This reverts commit 154bec589a03a642de9ba177e0872404f50f5e15.

--- src/main/java/com/bridgelabz/indianstatecensusanalyzer/StateCensusAnalyzer.java
+++ src/main/java/com/bridgelabz/indianstatecensusanalyzer/StateCensusAnalyzer.java
@@ -1,31 +1,9 @@
 package com.bridgelabz.indianstatecensusanalyzer;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Iterator;
-import java.util.stream.StreamSupport;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
 public class StateCensusAnalyzer {
-	public Path csvFilePath;
-
-	public StateCensusAnalyzer(Path csvFilePath) {
-		this.csvFilePath = csvFilePath;
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyzer Program");
 	}
 
-	public int readStateCensusCSVData() throws Exception {
-
-		try (Reader reader = Files.newBufferedReader(csvFilePath)) {
-			CsvToBeanBuilder<StateCensusCSV> builder = new CsvToBeanBuilder<StateCensusCSV>(reader);
-			CsvToBean<StateCensusCSV> csvToBean = builder.withType(StateCensusCSV.class)
-					.withIgnoreLeadingWhiteSpace(true).build();
-			Iterator<StateCensusCSV> StateCensusCSVIterator = csvToBean.iterator();
-			Iterable<StateCensusCSV> csvIterable = () -> StateCensusCSVIterator;
-			int noOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-			return noOfEnteries;
-		}
-	}
 }
