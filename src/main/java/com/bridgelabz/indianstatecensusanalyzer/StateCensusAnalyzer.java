d1b94945d61c9ab47592232fa2e4ab90b87df578
Aditya Verma <aaadarths2357@gmail.com>
2020-10-14 17:00:26 +0530

    Added: program with welcome message to check project configuration

---
+++ src/main/java/com/bridgelabz/indianstatecensusanalyzer/StateCensusAnalyzer.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.indianstatecensusanalyzer;
+
+public class StateCensusAnalyzer {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyzer Program");
+	}
+
+}
