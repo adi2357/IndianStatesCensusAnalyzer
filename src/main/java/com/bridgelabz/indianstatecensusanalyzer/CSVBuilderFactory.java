c7eeef671dc0fdc5bc690dd9529870cd2ed64c06
Aditya Verma <aaadarths2357@gmail.com>
2020-10-19 08:44:27 +0530

    Modified: removed common csv builder and add csv exception file

--- src/main/java/com/bridgelabz/indianstatecensusanalyzer/CSVBuilderFactory.java
+++ src/main/java/com/bridgelabz/indianstatecensusanalyzer/CSVBuilderFactory.java
@@ -1,15 +1,8 @@
 package com.bridgelabz.indianstatecensusanalyzer;
 
-import com.bridgelabz.opencsvbuilder.ICSVBuilder;
-import com.bridgelabz.opencsvbuilder.OpenCSVBuilder;
-
 public class CSVBuilderFactory {
 
-	public static <E> ICSVBuilder<E> createOpenCSVBuilder() {
+	public static <E> ICSVBuilder<E> createCSVBuilder() {
 		return new OpenCSVBuilder<E>();
-	}
-
-	public static <E> ICSVBuilder<E> createCommonsCSVBuilder() {
-		return new CommonsCSVBuilder<E>();
 	}
 }
